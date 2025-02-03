58959340d056184b21aa072989cf086405668fbf
Tanveer <tanveerpathan024@gmail.com>
2025-02-03 17:01:57 +0530

    Hook's how work

--- 02Hooks/src/App.jsx
+++ 02Hooks/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Counter from "./components/Counter"
 
 function App() {
   return (
-    <div className="w-[100vw] h-screen  bg-red-300 ">
-      hello
+    <div className="w-[100vw] h-screen  bg-gray-400 flex m-auto justify-center items-center">
+      <Counter />
     </div>
   );
 }
--- 02Hooks/src/components/Counter.jsx
+++ 02Hooks/src/components/Counter.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
-  return <div className=" flex flex-col  "></div>;
+  let [counter, setCounter] = useState(0);
+
+  let addValue = () => {
+    if (counter < 15) {
+      setCounter(counter + 1);
+    } else {
+      alert("You's reached limit");
+    }
+  };
+
+  let removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      alert("You's reached limit");
+    }
+  };
+
+  return (
+    <div className=" flex flex-col ">
+      <h1>Counter App Component</h1>
+
+      <div className=" py-10  flex justify-center items-center gap-5  ">
+        <button onClick={addValue} className="font-bold text-2xl bg-gray-600 p-3 text-white rounded-xl ">
+          Add
+        </button>
+        <h2 className="font-bold text-2xl">{counter}</h2>
+        <button onClick={removeValue} className="font-bold text-2xl bg-gray-600 p-3 text-white rounded-xl">
+          Remove
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
