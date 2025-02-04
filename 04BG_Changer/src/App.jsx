38d3e136231a228d8ffa4f6ec2718cd1d150a20f
Tanveer <tanveerpathan024@gmail.com>
2025-02-04 12:24:22 +0530

    i make color generator in react

--- 04BG_Changer/src/App.jsx
+++ 04BG_Changer/src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Button from "./components/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("olive");
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div
+      className=" bg-gray-600 h-screen w-full flex flex-col items-center justify-center"
+      style={{ backgroundColor: color }}
+    >
+      <h1 className="text-white mt-10 font-extrabold text-4xl">{color}</h1>
+      <div className="fixed bottom-1 bg-white rounded-4xl p-4 mb-4 flex gap-6">
+        <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-2 shadow-lg rounded-4xl text-amber-50"
+          style={{ backgroundColor: "red" }}
+        >
+          Red
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-2 shadow-lg rounded-4xl text-amber-50"
+          style={{ backgroundColor: "green" }}
+        >
+          Green
+        </button>
+        <button
+          className="outline-none px-4 py-2 shadow-lg rounded-4xl text-black"
+          style={{ backgroundColor: "yellow" }}
+          onClick={() => setColor("yellow")}
+        >
+          Yellow
+        </button>
+        <button
+          className="outline-none px-4 py-2 shadow-lg rounded-4xl text-amber-50"
+          style={{ backgroundColor: "orange" }}
+          onClick={() => setColor("orange")}
+        >
+          Orange
+        </button>
+        <button
+          className="outline-none px-4 py-2 shadow-lg rounded-4xl text-amber-50"
+          style={{ backgroundColor: "black" }}
+          onClick={() => setColor("black")}
+        >
+          Black
+        </button>
+        <button
+          className="outline-none px-4 py-2 shadow-lg rounded-4xl text-amber-50"
+          style={{ backgroundColor: "pink" }}
+          onClick={() => setColor("pink")}
+        >
+          Pink
+        </button>
+        <button
+          className="outline-none px-4 py-2 shadow-lg rounded-4xl text-amber-50"
+          style={{ backgroundColor: "blue" }}
+          onClick={() => setColor("blue")}
+        >
+          Blue
+        </button>
+
+        <button
+          className="outline-none px-4 py-2 shadow-lg rounded-4xl text-amber-50"
+          style={{ backgroundColor: "grey" }}
+          onClick={() => setColor("grey")}
+        >
+          Grey
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ 04BG_Changer/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({red}) => {
+  return (
+    <div className='bg-red-500'>
+        {red}
+    </div>
+  )
+}
+
+export default Button
