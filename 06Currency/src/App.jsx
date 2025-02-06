1946ed09cfcfcb105127280e48843673d28fb4b3
Tanveer <tanveerpathan024@gmail.com>
2025-02-06 13:32:57 +0530

    created currecy chager using  custom hook

--- 06Currency/src/App.jsx
+++ 06Currency/src/App.jsx
@@ -1,41 +1,73 @@
-
+import { useState } from "react";
+import InputBox from "./components/InputBox";
+import useCurrencyinfo from "./hooks/useCurrencyinfo";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyinfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setAmount(convertedAmount); // Assign previous convertedAmount to amount
+    setConvertedAmount(amount); // Keep the original amount as convertedAmount
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * (currencyInfo[to] || 0));
+  };
 
   return (
-    <div className="bg-blue-400  w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-10">Currency Conveter App</h1>
+    <div className="w-full h-screen bg-gray-500 flex flex-col items-center justify-center">
+      <div className="w-full max-w-md flex flex-col gap-y-2 border-2 bg-white/30 border-white p-5 backdrop-blur-md shadow-lg rounded-xl">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            convert();
+          }}
+          action=""
+        >
+          <div className="w-full mb-1">
+            <InputBox
+              label="From"
+              amount={amount}
+              currencyOptions={options}
+              onCurrencyChange={(currency) => setFrom(currency)}
+              selectedCurrency={from}
+              onAmountChange={(amount) => setAmount(Number(amount))}
+            />
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-white border-2 rounded-md bg-blue-500 text-white p-1"
+                onClick={swap}
+              >
+                Swap
+              </button>
+            </div>
 
-      <div className="bg-white/30 relative shadow-lg backdrop-blur-md w-full max-w-md border-[2px] border-white p-4 flex flex-col justify-between items-center gap-4 rounded-lg">
-        <div className="w-full bg-white flex justify-between items-center p-4 rounded-xl">
-          <div>
-            <p className="text-gray-600">From</p>
-            <h1>0</h1>
+            <InputBox
+              label="To"
+              amount={convertedAmount}
+              currencyOptions={options}
+              onCurrencyChange={(currency) => setTo(currency)}
+              selectedCurrency={to}
+              amountDisabled
+            />
           </div>
-          <div>
-            <p className="text-gray-600">Currency Type</p>
-            <h1>usd</h1>
-          </div>
-        </div>
-
-        <div className="w-full bg-white flex justify-between items-center p-4 rounded-xl">
-          <div>
-            <p className="text-gray-600">To</p>
-            <h1>0</h1>
-          </div>
-          <div>
-            <p className="text-gray-600">Currency Type</p>
-            <h1>ind</h1>
-          </div>
-        </div>
-
-        <div className="w-full cursor-pointer bg-blue-700 py-4 text-white text-center rounded-xl">
-          <h1 className="text-xl font-thin">Convert usd to ind</h1>
-        </div>
-        <div className="absolute bg-blue-600 text-white px-2 py-1 border-2 border-white top-[31%] rounded-lg">
-          {" "}
-          Swap{" "}
-        </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+          >
+            Convert {from.toUpperCase()} to {to.toUpperCase()}
+          </button>
+        </form>
       </div>
     </div>
   );
