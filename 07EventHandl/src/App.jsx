6eead6744972bc7c37ef1c94bf4f348ef6a5dd09
Tanveer <tanveerpathan024@gmail.com>
2025-02-09 16:32:52 +0530

    handel some error

--- 07EventHandl/src/App.jsx
+++ 07EventHandl/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import Button from './Components/Button'
-import ColorChg from './Components/ColorChg'
+import { useState } from "react";
+import Button from "./Components/Button";
+import ColorChg from "./Components/ColorChg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className=' w-full h-screen  text-white flex items-center justify-center '>
+    <div className=" w-full h-screen  text-white flex items-center justify-center ">
       {/* <Button /> */}
-      <ColorChg />
+      <div className="max-w-md h-[200px] p-4 border-[1px] border-black">
+        <ColorChg />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
