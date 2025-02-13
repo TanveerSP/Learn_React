6804f61e8e6014b15448ef10c74329fe56100841
Tanveer <tanveerpathan024@gmail.com>
2025-02-13 09:54:39 +0530

    deep learn how to useEffect work

--- 08useEffect_Hook/src/components/BasicuseEffect/DataFatcher.jsx
+++ 08useEffect_Hook/src/components/BasicuseEffect/DataFatcher.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const DataFatcher = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoadding] = useState(true);
 
-    const [data, setData] =useState([]);
-    const [loading, setLoadding] = useState(true);
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => {setData(data)
-            setLoadding(false);
-        })
-        .catch((err) => console.log("error come", err))
-    }, [])  
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoadding(false);
+      })
+      .catch((err) => console.log("error come", err));
+  }, []);
 
   return (
     <div>
-        { loading ? (<div>
-            <h1>Loadding....</h1>
-        </div> ) 
-        : (<div>
-         <ul>
-            {data.map(post => (
-                <li key={post.id}>
-                    {post.title}
-                </li>
+      {loading ? (
+        <div>
+          <h1>Loadding....</h1>
+        </div>
+      ) : (
+        <div>
+          <ul>
+            {data.map((post) => (
+              <li key={post.id}>{post.title}</li>
             ))}
-         </ul>
-        </div>)
-        }
+          </ul>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default DataFatcher
+export default DataFatcher;
