c05d18e9372b17cff00288be2ce3712ecd3885a2
Tanveer <tanveerpathan024@gmail.com>
2025-02-18 13:31:58 +0530

    practice navigate

--- 10react_router/src/components/About.jsx
+++ 10react_router/src/components/About.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const About = () => {
+
+  const navigate = useNavigate();
+  function handleNavigate() {
+    navigate('/student/abc123')
+  }
+
   return (
     <div>
       <h1>About Page</h1>
