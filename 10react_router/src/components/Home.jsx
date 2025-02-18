6c9fbef835d1efeeef4bf321969e5c6ae8d6f479
Tanveer <tanveerpathan024@gmail.com>
2025-02-18 13:30:25 +0530

    I learn about useNavigate

--- 10react_router/src/components/Home.jsx
+++ 10react_router/src/components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const Home = () => {
+
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate('/about');
+  }
+
   return (
     <div>
      <h1> Home Page</h1>
+
+     <button onClick={handleClick}>
+      Move to About page
+     </button>
     </div>
   )
 }
