163de2e8b0555e73025d96d229393d3a5b70ffb3
Tanveer <tanveerpathan024@gmail.com>
2025-02-19 16:14:33 +0530

    add How add delay to submit and how if user click submit button then disable submit button

--- 11HookForm/src/components/FormPage.jsx
+++ 11HookForm/src/components/FormPage.jsx
@@ -6,25 +6,32 @@ const FormPage = () => {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm();
 
+  // Api call ko simulate krna hai
+  async function onSubmit(data) {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("this is form data", data);
+  }
 
-  const onSubmit = (data) => console.log("this is form data", data);
+  //   const onSubmit = (data) => console.log("this is form data", data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="">First Name: </label>
-        <input 
-        className={errors.firstName ? 'input-error' : ""}
-        {...register("firstName", 
-            { required: true,
-             minLength: {value:4, message:"Min len atleast 3"} , 
-             maxLength: {value:7, message:"Max len atleast 7"}, 
-             
-            })} />
-            {errors.firstName && <p className='error-msg'>{errors.firstName.message}</p>}
+        <input
+          className={errors.firstName ? "input-error" : ""}
+          {...register("firstName", {
+            required: true,
+            minLength: { value: 4, message: "Min len atleast 3" },
+            maxLength: { value: 7, message: "Max len atleast 7" },
+          })}
+        />
+        {errors.firstName && (
+          <p className="error-msg">{errors.firstName.message}</p>
+        )}
       </div>
       <div>
         <label htmlFor="">Middle Name: </label>
@@ -32,17 +39,22 @@ const FormPage = () => {
       </div>
       <div>
         <label htmlFor="">Last Name: </label>
-        <input {...register("lastName", {pattern: {
-            value: /^[A-Za-z]+$/i,
-            message: "Last name is not as per the rules"
-        }})} />
-        {errors.lastName && 
-        <p className="error-msg">
-            {errors.lastName.message}
-        </p>
-        }
+        <input
+          {...register("lastName", {
+            pattern: {
+              value: /^[A-Za-z]+$/i,
+              message: "Last name is not as per the rules",
+            },
+          })}
+        />
+        {errors.lastName && (
+          <p className="error-msg">{errors.lastName.message}</p>
+        )}
       </div>
-      <input type="submit" />
+      <input type="submit" 
+      disabled={isSubmitting}
+      value={isSubmitting ? "Submtting" : "Submint" }
+      />
     </form>
   );
 };
