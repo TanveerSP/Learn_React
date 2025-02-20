6f6cd38a7a3f517ec43fbef6d56fe421140e9e25
Tanveer <tanveerpathan024@gmail.com>
2025-02-20 20:28:24 +0530

    practice form how to use useForm and if user click on submit btn then btn disapled on submiting process if process complit then button show . aoviding multipla click data at  one time

--- 11HookForm/src/App.jsx
+++ 11HookForm/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import FormPage from "./components/FormPage";
+import Practice2 from "./components/Practice2";
 import PracticeForm1 from "./components/PracticeForm1";
 
 function App() {
   return (
     <div>
       {/* <FormPage /> */}
-      <PracticeForm1 />
+      {/* <PracticeForm1 /> */}
+      <Practice2 />
     </div>
   );
 }
--- 11HookForm/src/components/Practice2.jsx
+++ 11HookForm/src/components/Practice2.jsx
@@ -1,18 +1,55 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Practice2 = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
 
-    const {
-        required,
-        handleSubmit,
-    } = useForm()
+  async function submitOn(data) {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log(data);
+  }
 
   return (
     <div>
-        
-    </div>
-  )
-}
+      <h2>Student Form</h2>
 
-export default Practice2
+      <form onSubmit={handleSubmit(submitOn)}>
+        <div>
+          <label>First Name: </label>
+          <input
+            type="text"
+            {...register("firstName", {
+              minLength: { value: 3, message: "Minmum 3 letter atlest" },
+              maxLength: { value: 12, message: "Maxmum 12 word required" },
+              required: true,
+            })}
+          />
+        </div>
+
+        {/* last name */}
+        <div>
+          <label>Last Name: </label>
+          <input
+            type="text"
+            {...register("lastName", {
+              minLength: { value: 3, message: "Minmum 3 letter atlest" },
+              maxLength: { value: 12, message: "Maxmum 12 word required" },
+              required: true,
+            })}
+          />
+        </div>
+        <input
+          disabled={isSubmitting}
+          value={isSubmitting ? "submitting.." : "submit"}
+          type="submit"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Practice2;
