55cd8ed851f7c7fdfb79936b5553739a11180020
Tanveer <tanveerpathan024@gmail.com>
2025-02-20 20:09:22 +0530

    practice of reac userForm

---
+++ 11HookForm/src/components/Practice2.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const Practice2 = () => {
+
+    const {
+        required,
+        handleSubmit,
+    } = useForm()
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Practice2
