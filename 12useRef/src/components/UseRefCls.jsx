41040a5d2a541967d8b1254c09b345a1e158a2c4
Tanveer <tanveerpathan024@gmail.com>
2025-02-21 16:00:13 +0530

    this code withoutt useRef

--- 12useRef/src/components/UseRefCls.jsx
+++ 12useRef/src/components/UseRefCls.jsx
@@ -4,22 +4,30 @@ import { useState } from "react";
 
 const UseRefCls = () => {
   const [count, setCount] = useState(0);
+  let val = 1;
+  
+  function handleCount() {
+    val = val + 1;
+    console.log("Value of val: ", val);
+    setCount(count + 1);
+  }
 
-  useEffect((0))
+  useEffect(() => {
+    console.log("Main frese render hogaya hu");
+  });
 
   return (
     <div>
       <h3>UseRefCls</h3>
       <button
-    //   onClick={() => setCount((prev) => prev + 1)}
+      onClick={handleCount}
+      //   onClick={() => setCount((prev) => prev + 1)}
       >
         Incresse
       </button>
 
       <div>
-        <h4>
-        Count: {count}
-        </h4>
+        <h4>Count: {count}</h4>
       </div>
     </div>
   );
