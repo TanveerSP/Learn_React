6f57d9c2898c6fa13d2a819c2dc2950a81c46a80
Tanveer <tanveerpathan024@gmail.com>
2025-03-02 12:50:16 +0530

    I learn how to use useRef

--- 12useRef/src/components/PracticeOne.jsx
+++ 12useRef/src/components/PracticeOne.jsx
@@ -1,18 +1,35 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const PracticeOne = () => {
   const countRef = useRef(0);
   const [count, setCount] = useState(0);
+  let val = countRef;
+
+  function handleCount() {
+    val.current = val.current + 1;
+    console.log("Value of val:", val.current);
+    setCount(count + 1);
+  }
+
+  function handleCountVal() {
+    val.current = val.current -1;
+    console.log("Value of val", val.current);
+    setCount((prev) => prev > 0 ?prev - 1 : 0)
+  }
+
+  //   it runs on every render
+  useEffect(() => {
+    console.log("count is ", count);
+  });
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Increes Value
-    </button>
+      <button onClick={handleCount}>Increes Value</button>
       <br />
       <br />
-      <button onClick={() => setCount((prev) => (prev > 0 ? prev - 1 : 0))}>
+      {/* <button onClick={() => setCount((prev) => (prev > 0 ? prev - 1 : 0))}> */}
+      <button onClick={handleCountVal}>
         Increes Value
       </button>
     </div>
