f56fd62c862ef833ccae6cf12660a94237d4ba4c
Tanveer <tanveerpathan024@gmail.com>
2025-03-06 13:01:47 +0530

    add update todo

--- reducxapp/src/components/Todo.jsx
+++ reducxapp/src/components/Todo.jsx
@@ -1,20 +1,56 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { removeTodo } from "../features/todo/todoSlice";
+import { removeTodo, updateTodo } from "../features/todo/todoSlice";
 
 const Todo = () => {
   const todos = useSelector((state) => state.todo.todos);
   const dispatch = useDispatch();
 
+  const [editId, setEditId] = useState(null);
+  const [editText, setEditText] = useState("");
+
+  const handleUpdate = (id) => {
+    if (editId.trim() === "") return;
+    dispatch(updateTodo({ id, newText: editText }));
+    setEditId(null);
+    setEditId("");
+  };
+
   return (
     <>
       <div>Todos</div>
-      {todos.map((ele) => (
-        <li key={ele.id}>
-          {ele.text}
-          <button onClick={() => dispatch(removeTodo(ele.id))}>delete</button>
-        </li>
-      ))}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {editId === todo.id ? (
+              <>
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                />
+                <button onClick={() => handleUpdate(todo.id)}>Save</button>
+                <button onClick={() => setEditId(null)}>Cancel</button>
+              </>
+            ) : (
+              <>
+                {todo.text}
+                <button onClick={() => dispatch(removeTodo(todo.id))}>
+                  Delete
+                </button>
+                <button
+                  onClick={() => {
+                    setEditId(todo.id);
+                    setEditText(todo.text);
+                  }}
+                >
+                  Edit
+                </button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
