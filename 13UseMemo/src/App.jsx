602c74a6cedd9b28f6fe4ebcbcc5a6881fa77c78
Tanveer <tanveerpathan024@gmail.com>
2025-03-08 14:46:06 +0530

    Practice useMemo

--- 13UseMemo/src/App.jsx
+++ 13UseMemo/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Counter from './components/Counter'
+import ExpensiveCal from './components/ExpensiveCal'
 
 function App() {
  
   return (
     <div>
        <h1>Learn useMemo Hook </h1>
-       <Counter />
+       {/* <Counter /> */}
+       <ExpensiveCal />
     </div>
   )
 }
---
+++ 13UseMemo/src/components/ExpensiveCal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpensiveCal = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ExpensiveCal
